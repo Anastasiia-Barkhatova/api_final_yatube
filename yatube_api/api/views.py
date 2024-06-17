1744a0b969f5f666cd217a522a5cc9f284ad9a1c
Anastasiia Barkhatova <nastyabarhatova@mail.ru>
2024-06-17 20:45:04 +0300

    17.06. с правками(1)

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -9,7 +9,7 @@ from .permissions import AuthorOrReadOnly
 from .serializers import (
     CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
 )
-from posts.models import Follow, Group, Post
+from posts.models import Group, Post
 
 
 class ListCreateViewSet(
